0e3745874497227d0c815bef367b7dc965671183
GlebLobinsky <Lobinsky.gleb@gmail.com>
2021-07-06 13:05:10 +0300

    creation of task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+
+class Task(models.Model):
+    User = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    start_time = models.DateTimeField(default=timezone.now, blank=True)
+    end_time = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=1), blank=True)
